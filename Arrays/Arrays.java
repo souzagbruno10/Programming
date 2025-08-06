dfe5a8037a092a5036e51f3b952e9332d3347c0b
Bruno Gomes de Souza <souzagbruno10@gmail.com>
2025-08-06 17:19:23 -0300

    array e garbage collector informacao

--- Arrays/Arrays.java
+++ Arrays/Arrays.java
@@ -15,6 +15,21 @@ public class Arrays {
             System.out.println("Comida - " + count + ": " + foods[i]);
         }
 
+        System.out.println("---------------------------------------------------------------------");
 
+        String[] musics = new String[5];
+
+        musics[0] = "Espadas y Serpentes";
+        musics[1] = "Hacelo por Mi";
+        musics[2] = "Vuelve a casa";
+        musics[3] = "America";
+        musics[4] = "El Cielo puede esperar";
+
+        for (int i = 0; i < 5; i++){
+            int counter = i;
+            System.out.println(counter + " - Música do A77aque que eu mais gosto: " + musics[i]);
+        }
     }
+// se sobrescrita a variavel do array ja criado em um novo tipo, o garbage collector apaga os dados anteriores da
+//memoria.
 }
